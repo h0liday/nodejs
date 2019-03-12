6582fbe813d1cf7904127b455240be44a2e96b77
h0l1day <yarechek.arabichek@gmail.com>
2019-03-12 09:48:52 +0100

    Node serv

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
