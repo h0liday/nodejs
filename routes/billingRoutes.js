906e16c1352092b4b957c4f483b3eef021898c44
h0l1day <yarechek.arabichek@gmail.com>
2019-03-25 10:39:46 +0100

    billing and client side app

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,16 +3,16 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin')
 
 module.exports = app => {
-    app.post('/api/stripe', requireLogin, async (req, res) => {
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: '$5 for 5 credits',
-            source: req.body.id
-        });
-        console.log(charge)
-        req.user.credits += 5;
-        const user = await req.user.save();
-        res.send(user)
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id
     });
+    console.log(charge);
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  });
 };
